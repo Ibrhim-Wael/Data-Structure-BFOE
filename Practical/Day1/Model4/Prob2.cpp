ebc7f471b076607dd5506232146bd6723f1de493
Ibrhim-Wael <ibrahemashhab@gmail.com>
2025-05-20 23:06:51 +0300

    Prob2.cpp

---
+++ Practical/Day1/Model4/Prob2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {5, 3, 8, 2, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before sorting: ";
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    cout << "After sorting: ";
+    printArray(arr, n);
+}
